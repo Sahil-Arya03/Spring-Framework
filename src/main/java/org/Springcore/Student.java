824eb163694a22ac7874d0b763a4e84a4802c70f
rocks <rockstar50906@gmail.com>
2026-04-30 17:59:36 +0530

    Spring

---
+++ src/main/java/org/Springcore/Student.java
@@ -0,0 +1,45 @@
+package org.Springcore;
+
+public class Student {
+    private int Studentid;
+    private String StudentName;
+    private String Address;
+
+    public Student(int studentid, String studentName, String address) {
+        super();
+        this.Studentid = studentid;
+        this.StudentName = studentName;
+        this.Address = address;
+    }
+
+    public Student() {
+        super();
+    }
+
+    public int getStudentid() {
+        return Studentid;
+    }
+
+    public void setStudentid(int studentid) {
+        Studentid = studentid;
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
+
+
+
